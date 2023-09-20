684933d27ea548d4e4980a48daaee95fadfc95fd
carl conrad <carlconrad98071234@gmail.com>
2023-09-21 02:05:12 +0800

    DynamicTableAndChair

--- TableAndChairPattern/TableAndChairs.java
+++ TableAndChairPattern/TableAndChairs.java
@@ -3,15 +3,18 @@ package TableAndChairPattern;
 
 class TableAndChairs{
 
-      public static void main(String[] args) {
-             
-            
+      static void DynamicTableAndChair(int x,int y){
+             if(x<5 || y<16) {
+                  System.out.println("Number of Rows must Greater than or equal 5 \nNumber of Columns must Greater than or equal 16");
+                  return;
+             }
+            int row = x;  
+            int column = y;
 
-            int row = 6;
-            int column = 32;
-     
+
             int interval = (column/4) + 1;
-            for (int i = 1; i <=row; i++) {
+            
+            for (int i = 0 - (row-8); i <=row; i++) {
                   for (int j = 1; j <=column; j++) {                          
                          if(  i <3   &&    (  j <2 || j > column-1 )){
                              System.out.print("X");
@@ -19,39 +22,22 @@ class TableAndChairs{
                                 System.out.print("X");                        
                          }else if(i == 4 && ( (j<=interval-2 )  ||  j == interval+1   ||  j == (column-(column/4))-1 || ( j >(column-(column/4))+1 )  )){
                                System.out.print("X"); 
-                        }else if(i >= 5 && (  j<2 || j == interval-2 || j ==   )){
-
+                        }else if(i >= 5 && (  j<2 || j == interval-2  || j == interval+1 ||  j==  (column-(column/4))-1 || j == (column-(column/4))+2 ) || j == column){
+                               System.out.print("X"); 
                         }else
                             System.out.print(" ");  
                          
                   }            // 1 ,5 8
               System.out.println();         
             }
-            
+      }
+
          
 
-
-
-      //  int row = 6;
-      //  int column =24;
-
-     
-      //  for (int i = 1; i <=row; i++) {
-      //        for (int j = 1; j <=column; j++) {                          
-      //               if(  i <3   &&    (  j == 1 || j == column )){
-      //                   System.out.print("X");
-      //               }else if(i == 3 &&  ( j<2 || j>7 ) &&  ( j< 18 || j>=24  )  ){
-      //                        System.out.print("X");                        
-      //               }else if(i == 4 && (j<=5 || j==8 || j == 17 || j >=20 )   ){
-      //                       System.out.print("X");   
-      //               }else if(i >= 5 && ( j<2 ||  (j>4&&j<6)  || (j>7 && j<9) || (j>16 && j<18)  || (j>19 && j<21) || (j>23) ) ) {
-      //                      System.out.print("X");   
-      //             }else
-      //                  System.out.print(" ");  
-                    
-      //        }            // 1 ,5 8
-      //    System.out.println();         
-      //  }
+      public static void main(String[] args) {
+             
+            DynamicTableAndChair(8, 30);
+        
        
       }
 
