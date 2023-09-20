aed09c7febbae3081903b3b18482d3ee6a975980
carl conrad <carlconrad98071234@gmail.com>
2023-09-21 00:20:43 +0800

    initial

--- TableAndChairPattern/TableAndChairs.java
+++ TableAndChairPattern/TableAndChairs.java
@@ -17,12 +17,12 @@ class TableAndChairs{
                              System.out.print("X");                        
                     }else if(i == 4 && (j<=5 || j==8 || j == 17 || j >=20 )   ){
                             System.out.print("X");   
-                    }else if(i >= 5 && ( j<2 ||  j== 4  || j == 8  || j == 17  ||  j==19 || j == 24 ) ) {
+                    }else if(i >= 5 && ( j<2 ||  (j>4&&j<6)  || (j>7 && j<9) || (j>16 && j<18)  || (j>19 && j<21) || (j>23) ) ) {
                            System.out.print("X");   
                   }else
                        System.out.print(" ");  
                     
-             }            
+             }            // 1 ,5 8
          System.out.println();         
        }
        
